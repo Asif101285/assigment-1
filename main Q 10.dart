279e18b25a6de879f4a82d496ebec40355ca2a4b
mohammad asif <asifumer974@gmail.com>
2024-01-17 14:26:27 +0500

    complete

--- main Q 10.dart
+++ main Q 10.dart
@@ -1,39 +1,39 @@
+// Q10: Write a program that takes three numbers from the user and prints the
+// greatest number & lowest number.
 
-
+import 'dart:io';
 
 void main() {
+  
+  print("Enter numbers:");
+  
+  print("Enter the first number:");
+  int num1 = int.parse(stdin.readLineSync()!);
 
-//Q10: Write a program that takes three numbers from the user and prints the
-//greatest number & lowest number
-  num a = 5;
-  num b = 90;
-  num c = 3;
+  print("Enter the second number:");
+  int num2 = int.parse(stdin.readLineSync()!);
 
-  if (a > b && a > c) {
-    print("$a IS GREATER THAN $b AND $c");
-    if (b < c) {
-      print("$b IS LOWEST NUMBER");
-    }
-     else {
-      print("$c IS LOWEST NUMBER");
-    }
+  print("Enter the third number:");
+  int num3 = int.parse(stdin.readLineSync()!);
+
+  // Find the greatest number using a conditional statement
+  int greatest = num1;
+  if (num2 > greatest) {
+    greatest = num2;
   }
-   else if (b > a && b > c) {
-    print("$b IS GREATER THAN $a AND $c");
-    if (a < c) {
-      print("$a IS LOWEST NUMBER");
-    } 
-    else {
-      print("$c IS LOWEST NUMBER");
-    }
+  if (num3 > greatest) {
+    greatest = num3;
   }
-   else {
-    print("$c IS GREATER THAN $a AND $b");
-    if (a < b) {
-      print("$a IS LOWEST NUMBER");
-    }
-   else {
-      print("$b IS LOWEST NUMBER");
-    }
-  }
+
+  // Find the lowest number using a conditional statement
+  int lowest = num1;
+  if (num2 < lowest) {
+    lowest = num2;
+  }
+  if (num3 < lowest) {
+    lowest = num3;
+  }
+
+  print("Greatest number: $greatest");
+  print("Lowest number: $lowest");
 }
